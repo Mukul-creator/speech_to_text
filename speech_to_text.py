24efef07b0d074d030d07db0093f3ea1f72c325e
Mukul-creator <Mukulkumar2652@gmail.com>
2020-10-26 13:36:15 +0530

    Adding listensting statement and language of speech

--- speech_to_text.py
+++ speech_to_text.py
@@ -3,5 +3,6 @@ sr = s.Recognizer()
 mic = s.Microphone()
 with mic as source:
     sr.adjust_for_ambient_noise(source)
+    print("I am listening you...")
     audio = sr.listen(source,timeout = 10)
-    print(sr.recognize_google(audio))
+    print(sr.recognize_google(audio,language ='en-IN'))
